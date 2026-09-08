afad37ba34314c49d02b777c7e0fdef218b61a3d
viking7474 <baolinhng9494@gmail.com>
2026-09-08 01:44:07 -0700

    fix ipv4

--- common/NetworkManager.m
+++ common/NetworkManager.m
@@ -55,9 +55,9 @@
         // Loop through linked list of interfaces
         temp_addr = interfaces;
         while (temp_addr != NULL) {
-            if (temp_addr->ifa_addr->sa_family == AF_INET) {
+            if (temp_addr->ifa_addr && temp_addr->ifa_addr->sa_family == AF_INET) {
                 // Check if interface is en0 which is the wifi connection on iOS
-                if ([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {
+                if (temp_addr->ifa_name && [[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {
                     // Get NSString from C String
                     address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
                     break;
